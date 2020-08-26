da2d992a7e2af7ccf732a3c41ac53920becdaab6
wanghaoyu <wanghaoyu@zhuoyuan-info.com>
2020-08-26 18:19:53 +0800

    'undone'

---
+++ src/component/upload.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="m_19_upload_wapper">
+    <input class="m_19_upload" id="upload" type="file" :multiple="multiple" />
+    <div class="m_19_upload_button" for="upload">{{content}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "19upload",
+  props: {
+    multiple: Boolean,
+    // image/gif, image/jpeg
+    accept: String,
+    content: {
+      type: String,
+      default: "上传图片",
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.m_19_upload_wapper {
+  display: inline-block;
+}
+.m_19_upload {
+  display: none;
+}
+.m_19_upload_button {
+  background: red;
+}
+</style>
---
+++ src/undone/checkbox.vue
@@ -0,0 +1,112 @@
+<template>
+  <label :for="id" class="label">
+    <span>
+      <slot />
+    </span>
+    <input
+      :id="id"
+      class="checkbox"
+      ref="checkbox"
+      type="checkbox"
+      :disabled="disabled"
+      v-model="currentValue"
+      @change="change"
+    />
+    <span class="title">显示</span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: "checkbox",
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    value: {
+      type: [String, Number, Boolean],
+      default: false,
+    },
+    trueValue: {
+      type: [String, Number, Boolean],
+      default: true,
+    },
+    falseValue: {
+      type: [String, Number, Boolean],
+      default: false,
+    },
+    indeterminate: {
+      type: Boolean,
+    },
+    id: {
+      type: Number | String,
+    },
+  },
+  data() {
+    return {
+      currentValue: this.value,
+    };
+  },
+  watch: {
+    // 同步修改currentValue，外部代码改变值内部更新
+    value(n) {
+      if (n !== this.trueValue && n !== this.falseValue) {
+        throw "Value should be trueValue or falseValue.";
+      } else {
+        this.updateModel();
+      }
+    },
+    indeterminate: {
+      immediate: true,
+      handler(n) {
+        this.$nextTick(() => {
+          const input = this.$refs?.checkbox;
+          if (this.value === this.falseValue && this.indeterminate) {
+            input.indeterminate = true;
+          } else {
+            input.indeterminate = false;
+          }
+        });
+      },
+    },
+  },
+  methods: {
+    updateModel() {
+      this.currentValue = this.value === this.trueValue;
+    },
+    // 向外触发改变外部值
+    change(event) {
+      if (this.disabled) {
+        return false;
+      }
+      // 当前选中状态
+      const checked = event.target.checked;
+      // 内部改变选中 改变内部属性
+      this.currentValue = checked;
+      // 向外触发
+      const value = checked ? this.trueValue : this.falseValue;
+      this.$emit("input", value); // v-model使用
+      this.$emit("on-change", value); // 正常触值使用
+    },
+  },
+};
+</script>
+
+<style>
+.label {
+  display: inline-flex;
+  flex-direction: row-reverse;
+  height: 16px;
+  line-height: 16px;
+}
+.checkbox ~ .title {
+  color: red;
+}
+input[type="checkbox"]:checked ~ span {
+  /* background-color: blue; */
+  /* color: blue; */
+  color: red;
+  background-color: yellowgreen;
+}
+</style>
